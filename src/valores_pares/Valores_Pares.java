4aafe839210bd656319ffbaadacefd6ff423ad74
Gabriel8458 <73495284+Gabriel8458@users.noreply.github.com>
2020-11-22 16:43:27 -0300

    Loop para numeros pares

---
+++ src/valores_pares/Valores_Pares.java
@@ -0,0 +1,14 @@
+package valores_pares;
+public class Valores_Pares {
+    public static void main(String[] args) {
+        int i;
+        
+        for(i=1000; i<1501; i++)
+        {
+          if(i%2==0)
+          {
+              System.out.println(i);
+          }
+          }
+        }
+    }
